5654f6e5bc456f3357b4c51a639c808714001c42
Peter Krentel <peterkrentel@gmail.com>
2025-04-03 09:52:17 -0500

    parking this

--- aws_cdk_ec2/aws_cdk_ec2_stack.py
+++ aws_cdk_ec2/aws_cdk_ec2_stack.py
@@ -2,7 +2,11 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_s3 as s3,
+    aws_iam as iam,
 )
+import aws_cdk as cdk
 from constructs import Construct
 
 class AwsCdkEc2Stack(Stack):
@@ -11,9 +15,58 @@ class AwsCdkEc2Stack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        # Example VPC
+        vpc = ec2.Vpc(self, "MyVpc",
+            max_azs=1  # Default is all AZs in the region
+        )
+        # Example Security Group
+        security_group = ec2.SecurityGroup(self, "MySecurityGroup",
+            vpc=vpc,
+            allow_all_outbound=True,
+            description="Allow SSH access",
+            security_group_name="MySecurityGroup"
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkEc2Queue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Lookup an existing key pair by name
+        key_pair = ec2.KeyPair.from_key_pair_attributes(self, "MyKeyPair",
+            key_pair_name="my-key-pair-ohio",  # Replace with your key pair name
+        )
+
+        # Example EC2 Instance
+        instance = ec2.Instance(self, "MyInstance",
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=ec2.MachineImage.latest_amazon_linux2023(),
+            vpc=vpc,
+            security_group=security_group,
+            key_pair=key_pair
+        )
+        # Example EBS Volume
+        volume = ec2.CfnVolume(self, "MyVolume",
+            availability_zone=vpc.availability_zones[0],
+            size=8,  # Size in GiB
+            volume_type="gp2"  # General Purpose SSD
+        )
+        # Attach the volume to the instance
+        ec2.CfnVolumeAttachment(self, "MyVolumeAttachment",
+            instance_id=instance.instance_id,
+            volume_id=volume.ref,
+            device="/dev/sdh"  # Device name
+        )
+        # Example S3 Bucket
+        
+        bucket = s3.Bucket(self, "MyBucket",
+            versioned=True,
+            removal_policy=cdk.RemovalPolicy.DESTROY,  # NOT recommended for production code
+            auto_delete_objects=True  # NOT recommended for production code
+        )
+        # Example IAM Role
+        
+        role = iam.Role(self, "MyRole",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3ReadOnlyAccess")
+            ]
+        )
+        # Attach the role to the instance
+        instance.role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3ReadOnlyAccess"))
+
